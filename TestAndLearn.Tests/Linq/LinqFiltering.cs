ee42b08ed6c96a360e3bf5202ef336418426ead7
Yongli, Shan <sylyongli@gmail.com>
2018-04-25 13:17:22 +0800

    parallelLoopState to stop individual task

---
+++ TestAndLearn.Tests/Linq/LinqFiltering.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TestAndLearn.Tests.Linq
+{
+    [TestFixture()]
+    class LinqFiltering
+    {
+        [Test]
+        public void Should_Return_Filtered_Result()
+        {
+            var source = new List<string>();
+            Enumerable.Range(0, 3).ToList().ForEach(x=>source.Add("abc"+x));
+            var filteredsource = from result in source
+                where result.EndsWith("2")
+                select result;
+            Assert.AreEqual(1,filteredsource.Count());
+        }
+    }
+}
---
+++ TestAndLearn.Tests/TPL/ParalleForUsage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace TestAndLearn.Tests.TPL
+{
+    [TestFixture()]
+    class ParalleForUsage
+    {
+        private static List<int> resource = Enumerable.Range(0, 160).ToList();
+        [Test]
+        public void DownloadParalle()
+        {
+            var result = new List<string>();
+            Parallel.For(0, 8, (x,loopstate) =>
+            {
+                var start = x * 50;
+                var end = DownloadFrom(start);
+                if (end >= start)
+                {
+                    var tmpResult = $"i am the source start {start} to end {end}";
+                    result.Add(tmpResult);
+                }
+                else
+                {
+                    var tmpResult = $"i am stopped on {x}";
+                    result.Add(tmpResult);
+                    loopstate.Stop();
+                }
+            });
+
+            TestContext.WriteLine(JsonConvert.SerializeObject(result,Formatting.Indented));
+        }
+
+        private int DownloadFrom(int number)
+        {
+            return Math.Min(number + 49, resource.Max());
+        }
+    }
+}
